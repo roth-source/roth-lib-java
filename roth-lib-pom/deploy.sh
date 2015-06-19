91ced21ddb3dd732d5a5d6676876e2feacbed43e
jaredrowell <jaredrowell@gmail.com>
2015-06-18 20:37:04 -0600

    init

--- roth-lib-pom/deploy.sh
+++ roth-lib-pom/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-mvn release:update-versions -DautoVersionSubmodules=true;
 mvn -N install;
 mvn -N -f ../roth-lib-api-pom/pom.xml install;
 mvn -N -f ../roth-lib-all/pom.xml install;
-#mvn -T 4 deploy;
+mvn -T 4 deploy;
