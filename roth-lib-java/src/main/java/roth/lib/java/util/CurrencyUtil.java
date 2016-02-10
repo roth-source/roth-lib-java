48b3dad82c37f4c373bd2c79bd8973ab1260d369
jaredrowell <jaredrowell@gmail.com>
2016-02-10 15:00:45 -0700

    update

--- roth-lib-java/src/main/java/roth/lib/java/util/CurrencyUtil.java
+++ roth-lib-java/src/main/java/roth/lib/java/util/CurrencyUtil.java
@@ -2,6 +2,7 @@ package roth.lib.java.util;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.text.DecimalFormat;
 
 public class CurrencyUtil
 {
@@ -11,9 +12,15 @@ public class CurrencyUtil
 		
 	}
 	
-	public String format(Integer value)
+	public static String format(Integer value)
 	{
-		return value != null ? new BigDecimal(BigInteger.valueOf(value), 2).toString() : null;
+		DecimalFormat formatter = new DecimalFormat("'$'#,##0.00");
+		return value != null ? formatter.format(new BigDecimal(BigInteger.valueOf(value), 2)) : null;
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println(format(123456789));
 	}
 	
 }
