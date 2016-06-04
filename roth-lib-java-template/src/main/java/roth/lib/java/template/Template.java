43f84e2cfb04a69fdebd542aa851309bbe40aeb5
jaredrowell <jaredrowell@gmail.com>
2016-06-04 16:14:08 -0600

    update

--- roth-lib-java-template/src/main/java/roth/lib/java/template/Template.java
+++ roth-lib-java-template/src/main/java/roth/lib/java/template/Template.java
@@ -1,10 +1,14 @@
 package roth.lib.java.template;
 
+import java.util.Map;
+
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+import roth.lib.java.json.JsonMapper;
+import roth.lib.java.mapper.MapperConfig;
 import roth.lib.java.util.ResourceUtil;
 
 public class Template
@@ -12,10 +16,10 @@ public class Template
 	protected static final String ENGINE			= "nashorn";
 	protected static final String ROTH_JS			= "roth-lib-js.js";
 	protected static final String ROTH_JS_TEMPLATE	= "roth-lib-js-template.js";
-	protected static final String ROTH_JS_JAVA		= "roth-lib-js-java.js";
 	protected static final String VERSION			= "$_version";
 	protected static final String CONFIG			= "$_config";
 	protected static final String TEMPLATE			= "$_template";
+	protected static final String JSON				= "JSON";
 	protected static final String PARSE				= "parse";
 	protected static final String EVAL				= "eval";
 	protected static final String RENDER			= "render";
@@ -23,8 +27,10 @@ public class Template
 	protected static final String SET_TEMPLATE		= String.format("var %s = new roth.lib.js.template.Template(" + CONFIG + ");", TEMPLATE);
 	
 	protected ScriptEngine engine;
-	protected Object template;
 	protected String version;
+	protected Object template;
+	protected Object json;
+	protected JsonMapper jsonMapper;
 	
 	public Template()
 	{
@@ -43,11 +49,30 @@ public class Template
 			engine = new ScriptEngineManager().getEngineByName(ENGINE);
 			engine.eval(ResourceUtil.toString(ROTH_JS));
 			engine.eval(ResourceUtil.toString(ROTH_JS_TEMPLATE));
-			engine.eval(ResourceUtil.toString(ROTH_JS_JAVA));
 			engine.put(CONFIG, config);
 			engine.eval(SET_VERSION + SET_TEMPLATE);
 			version = (String) engine.get(VERSION);
 			template = engine.get(TEMPLATE);
+			json = engine.get(JSON);
+			jsonMapper = new JsonMapper(new MapperConfig().setSerializeNulls(true));
+		}
+		catch(ScriptException e)
+		{
+			throw new TemplateException(e);
+		}
+	}
+	
+	public Template setJsonMapper(JsonMapper jsonMapper)
+	{
+		this.jsonMapper = jsonMapper;
+		return this;
+	}
+	
+	public void dependency(String source)
+	{
+		try
+		{
+			engine.eval(source);
 		}
 		catch(ScriptException e)
 		{
@@ -68,11 +93,41 @@ public class Template
 		}
 	}
 	
-	public String eval(String parsedSource, Object data)
+	protected Object parseJson(String value)
 	{
 		try
 		{
-			Object rendered = ((Invocable) engine).invokeMethod(template, EVAL, parsedSource, data);
+			return ((Invocable) engine).invokeMethod(json, PARSE, value);
+		}
+		catch(NoSuchMethodException | ScriptException e)
+		{
+			throw new TemplateException(e);
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	protected String serializeJson(Object scope)
+	{
+		if(scope instanceof Map)
+		{
+			return jsonMapper.serialize((Map<String, ?>) scope);
+		}
+		else
+		{
+			return jsonMapper.serialize(scope);
+		}
+	}
+	
+	protected Object convertJson(Object scope)
+	{
+		return parseJson(serializeJson(scope));
+	}
+	
+	public String eval(String parsedSource, Object scope)
+	{
+		try
+		{
+			Object rendered = ((Invocable) engine).invokeMethod(template, EVAL, parsedSource, convertJson(scope));
 			return rendered != null ? rendered.toString() : "";
 		}
 		catch(NoSuchMethodException | ScriptException e)
@@ -81,11 +136,11 @@ public class Template
 		}
 	}
 	
-	public String render(String source, Object data)
+	public String render(String source, Object scope)
 	{
 		try
 		{
-			Object rendered = ((Invocable) engine).invokeMethod(template, RENDER, source, data);
+			Object rendered = ((Invocable) engine).invokeMethod(template, RENDER, source, convertJson(scope));
 			return rendered != null ? rendered.toString() : "";
 		}
 		catch(NoSuchMethodException | ScriptException e)
