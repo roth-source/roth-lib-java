04d1db104b451c083f859ca40216723f8ec4846a
jaredrowell <jaredrowell@gmail.com>
2016-07-18 22:47:58 -0600

    update

--- roth-lib-java-http/src/main/java/roth/lib/java/http/HttpClient.java
+++ roth-lib-java-http/src/main/java/roth/lib/java/http/HttpClient.java
@@ -5,6 +5,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
+import javax.net.ssl.HttpsURLConnection;
+
 import roth.lib.java.inputter.Inputter;
 
 public class HttpClient
@@ -20,12 +22,21 @@ public class HttpClient
 		URLConnection connection = new URL(url.toString()).openConnection();
 		if(connection instanceof HttpURLConnection)
 		{
+			if(connection instanceof HttpsURLConnection)
+			{
+				setSslSocketFactory((HttpsURLConnection) connection);
+			}
 			return new HttpConnection(url, (HttpURLConnection) connection, debug);
 		}
 		else
 		{
 			throw new HttpException("unable to cast connection to HttpURLConnection");
 		}
+	}
+	
+	protected void setSslSocketFactory(HttpsURLConnection connection)
+	{
+		
 	}
 	
 	public <T> HttpResponse<T> connect(HttpRequest<?> request, Inputter<T> inputter, boolean debug) throws IOException
