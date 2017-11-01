b3ee83fa9a1992293deff05a6207ee82ec098a0f
Jared Rowell <jaredrowell@gmail.com>
2017-11-01 16:09:04 -0600

    update

---
+++ roth-lib-java/src/main/java/roth/lib/java/util/ChecksumUtil.java
@@ -0,0 +1,32 @@
+package roth.lib.java.util;
+
+import java.util.zip.CRC32;
+
+public class ChecksumUtil
+{
+	
+	protected ChecksumUtil()
+	{
+		
+	}
+	
+	public long checksumCrc32(String value)
+	{
+		return checksumCrc32(value.getBytes());
+	}
+	
+	public long checksumCrc32(byte[] bytes)
+	{
+		try
+		{
+			CRC32 crc32 = new CRC32();
+			crc32.update(bytes);
+			return crc32.getValue();
+		}
+		catch(Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
